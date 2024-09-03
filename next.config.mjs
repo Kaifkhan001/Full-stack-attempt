9939737c9988a97e1d56814cc0ae7f07862e5b79
Kaifkhan001 <kaifkhanksi7544@gmail.com>
2024-09-04 00:00:49 +0530

    next-config Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    exportPathMap: async function (defaultPathMap) {
-        return {
-          '/': { page: '/' },
-          '/about-us': { page: '/about-us' },
-          '/contact-us': { page: '/contact-us' },
-          // Add other pages here
-        };
-      },
-};
+}
 
   
 
